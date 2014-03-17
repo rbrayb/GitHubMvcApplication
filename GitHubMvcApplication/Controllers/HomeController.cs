dd199f0a4388327459ab956003c528a47f68ce17
rbrayb <rbrayb@gmail.com>
2014-03-17 14:21:35 +1300

    Changed message

--- GitHubMvcApplication/Controllers/HomeController.cs
+++ GitHubMvcApplication/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace GitHubMvcApplication.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "RB  - Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
         }
